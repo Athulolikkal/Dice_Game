1ebaadaef5219da787703a93c46830c8d4f70f47
Athulolikkal <athulolikkal@gmail.com>
2024-06-05 09:47:13 +0530

    installed dependencis for the backend and created an route

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import resultController from "./controllers/result/result_controller.js";
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(urlencoded({ extended: true }));
+
+app.post("/getresult", resultController.checkReult);
+
+app.listen(3000, () => {
+  console.log("port is running on 3000");
+});
---
+++ backend/controllers/result/result_controller.js
@@ -0,0 +1,7 @@
+const resultController = {
+    checkReult:async(req,res)=>{
+       console.log('call came to result controller') 
+    }
+}
+
+export default resultController;
